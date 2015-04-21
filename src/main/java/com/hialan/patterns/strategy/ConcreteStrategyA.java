4203cf5b138e645be73c40ef71e3798108a55101
Alan <alanlhy@gmail.com>
2015-04-21 23:21:55 +0800

    added strategy

---
+++ src/main/java/com/hialan/patterns/strategy/ConcreteStrategyA.java
@@ -0,0 +1,12 @@
+package com.hialan.patterns.strategy;
+
+/**
+ * User: Alan
+ * Email:alan@hialan.com
+ * Date: 4/21/15 23:19
+ */
+public class ConcreteStrategyA implements IStrategy {
+	public void doSomething() {
+
+	}
+}
---
+++ src/main/java/com/hialan/patterns/strategy/IStrategy.java
@@ -0,0 +1,10 @@
+package com.hialan.patterns.strategy;
+
+/**
+ * User: Alan
+ * Email:alan@hialan.com
+ * Date: 4/21/15 23:19
+ */
+public interface IStrategy {
+	void doSomething();
+}
